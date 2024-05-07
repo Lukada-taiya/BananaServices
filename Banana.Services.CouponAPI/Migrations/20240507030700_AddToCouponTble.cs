6a407e57d47757c3f3fd3b02b7e84ee75fa683a0
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-07 11:58:06 +0000

    Initial Commit

---
+++ Banana.Services.CouponAPI/Migrations/20240507030700_AddToCouponTble.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Banana.Services.CouponAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToCouponTble : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "CouponId", "CouponCode", "DiscountAmount", "MinAmount" },
+                values: new object[,]
+                {
+                    { 1, "A233D", 15.0, 30 },
+                    { 2, "F33E2", 35.0, 60 },
+                    { 3, "S345G", 15.0, 45 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Coupons",
+                keyColumn: "CouponId",
+                keyValue: 3);
+        }
+    }
+}
