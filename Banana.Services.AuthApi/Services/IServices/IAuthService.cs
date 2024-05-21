98661711c6475b3fe50ee36a8e382675d140a12a
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-21 11:18:43 +0000

    Feat: Implemented authservice to manage auth logic.

---
+++ Banana.Services.AuthApi/Services/IServices/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using Banana.Services.AuthApi.Models.Dto;
+
+namespace Banana.Services.AuthApi.Services.IServices
+{
+    public interface IAuthService
+    {
+        Task<string> Register(RegisterDto registerDto);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequest);
+        Task<bool> AssignRole(string email, string roleName);
+    }
+}
