5d0ae36830084d55365b55ac45e2011cc09d9ec8
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-21 01:13:48 +0000

    Feat: Added neccessary dtos to auth

---
+++ Banana.Services.AuthApi/Models/Dto/RegisterDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Banana.Services.AuthApi.Models.Dto
+{
+    public class RegisterDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string EmailPhuneNumber { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ Banana.Services.AuthApi/Models/Dto/ResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Banana.Services.AuthApi.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess { get; set; } = true;
+        public string Message { get; set; } = "";
+    }
+}
