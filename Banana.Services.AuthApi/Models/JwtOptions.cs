bf1a7350472001c272f9748b5a2ffad7f1ad68c3
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-21 01:15:22 +0000

    Feat: Added Jwt Support for auth

---
+++ Banana.Services.AuthApi/Models/JwtOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Banana.Services.AuthApi.Models
+{
+    public class JwtOptions
+    {
+        public string Issuer { get; set; } = string.Empty;
+        public string Audience { get; set; } = String.Empty;
+        public string Secret { get; set; } = String.Empty;
+    }
+}
