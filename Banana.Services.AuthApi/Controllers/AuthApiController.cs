b8d3064b95e197c87580be0111eb29c5ea5e4ea6
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-21 01:19:28 +0000

    Feat: Added auth api controller

---
+++ Banana.Services.AuthApi/Controllers/AuthApiController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banana.Services.AuthApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthApiController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return Ok();
+        }
+    }
+}
