9e68636612d89bac70e26f927ba60625c758544c
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-21 11:19:21 +0000

    Feat: Implemented feature to generate login user token

---
+++ Banana.Services.AuthApi/Services/JwtGenerator.cs
@@ -0,0 +1,38 @@
+﻿using Banana.Services.AuthApi.Models;
+using Banana.Services.AuthApi.Services.IServices;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Banana.Services.AuthApi.Services
+{
+    public class JwtGenerator(IOptions<JwtOptions> jwtOptions) : IJwtGenerator
+    {
+        private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+        public string GenerateToken(ApplicationUser user)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_jwtOptions.Secret);
+            var claimList = new List<Claim>()
+            {
+                new (JwtRegisteredClaimNames.Email, user.Email),
+                new (JwtRegisteredClaimNames.Sub, user.Id),
+                new (JwtRegisteredClaimNames.Name, user.UserName)
+        };
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claimList),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+         
+    }
+}
