b7443a7178ecfccb2da02f613e1971c5fe8c48f3
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-22 18:59:56 +0000

    Feat: Added relevant auth settings - Program.cs

--- Banana.Services.CouponAPI/Program.cs
+++ Banana.Services.CouponAPI/Program.cs
@@ -1,7 +1,12 @@
 using AutoMapper;
 using Banana.Services.CouponAPI;
 using Banana.Services.CouponAPI.Data;
+using Banana.Services.CouponAPI.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +25,31 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt => {
+    opt.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorisation",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference =  new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id =  JwtBearerDefaults.AuthenticationScheme
+                }
+            }, new string[]{}
+        }
+    }) ; 
+});
+
+builder.AddAppAuth();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -32,7 +61,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
