36ee5dbd521afeae22165780b024a479dc2571e2
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-22 19:00:38 +0000

    Enhancement: Separated auth logic to keep Program.cs lean

---
+++ Banana.Services.CouponAPI/Extensions/WebAppBuilderExtension.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace Banana.Services.CouponAPI.Extensions
+{
+    public static class WebAppBuilderExtension
+    {
+        public static WebApplicationBuilder AddAppAuth(this WebApplicationBuilder builder) 
+        {
+            var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
+            var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+            var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
+            var key = Encoding.ASCII.GetBytes(secret);
+            builder.Services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(o =>
+            {
+                o.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience
+                };
+            });
+            return builder;
+        }
+    }
+}
