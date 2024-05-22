6ebae4c0ec53168a91cfa1623013fd04ef75e373
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-22 19:03:33 +0000

    Feat: Added auth neccessary dtos

---
+++ Banana.Web/Models/LoginRequestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Banana.Web.Models.Dto
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
