94c5d46375085afc0e4ff90329f064890d206025
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-27 17:19:45 +0000

    Feat: Added automapper config for products

---
+++ Banana.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Banana.Services.CouponAPI.Models;
+using Banana.Services.CouponAPI.Models.Dto;
+using Banana.Services.ProductAPI.Models;
+using Banana.Services.ProductAPI.Models.Dto;
+
+namespace Banana.Services.ProductAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
