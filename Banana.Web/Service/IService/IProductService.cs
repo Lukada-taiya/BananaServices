1c3a07e4dea4032c04cde58b2f607570558f1f90
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-27 17:24:04 +0000

    Feat: Implemented services for product

---
+++ Banana.Web/Service/IService/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using Banana.Web.Models;
+
+namespace Banana.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto> GetProductAsync(int id);
+        Task<ResponseDto> GetAllProductsAsync();
+        Task<ResponseDto> GetProductByNameAsync(string name);
+        Task<ResponseDto> CreateProductAsync(ProductDto productDto);
+        Task<ResponseDto> UpdateProductAsync(ProductDto productDto);
+        Task<ResponseDto> DeleteProductAsync(int id);
+    }
+}
---
+++ Banana.Web/Service/ProductService.cs
@@ -0,0 +1,67 @@
+﻿using Banana.Web.Models;
+using Banana.Web.Service.IService;
+using static Banana.Web.Utility.StaticData;
+
+namespace Banana.Web.Service
+{
+    public class ProductService(IBaseService baseService) : IProductService
+    {
+        private readonly IBaseService _service = baseService;
+
+        public async Task<ResponseDto> CreateProductAsync(ProductDto productDto)
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = productDto,
+                Url = ProductApiBase + "/api/product"
+            });
+        }
+
+        public async Task<ResponseDto> DeleteProductAsync(int id)
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.DELETE,
+                Url = ProductApiBase + "/api/product/" + id
+            });
+        }
+
+        public async Task<ResponseDto> GetAllProductsAsync()
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductApiBase + "/api/product"
+            });
+        }
+
+        public async Task<ResponseDto> GetProductAsync(int id)
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductApiBase + "/api/product/" + id
+            });
+        }
+
+        public async Task<ResponseDto> GetProductByNameAsync(string name)
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = ProductApiBase + "/api/product/GetByName/" + name
+            });
+        }
+
+        public async Task<ResponseDto> UpdateProductAsync(ProductDto productDto)
+        {
+            return await _service.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.PUT,
+                Url = ProductApiBase + "/api/product",
+                Data = productDto
+            });
+        }
+    }
+}
