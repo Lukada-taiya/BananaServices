804fddaff42c8ec8e15aaf78cc13bf74442b3a51
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-27 17:23:10 +0000

    Feat: Implemented crud endpoints for products

---
+++ Banana.Services.ProductAPI/Controllers/ProductsApiController.cs
@@ -0,0 +1,121 @@
+﻿using AutoMapper;
+using Banana.Services.CouponAPI.Models.Dto;
+using Banana.Services.ProductAPI.Data;
+using Banana.Services.ProductAPI.Models;
+using Banana.Services.ProductAPI.Models.Dto;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Banana.Services.ProductAPI.Controllers
+{
+    [Route("api/product")]
+    [ApiController]
+    [Authorize]
+    public class ProductsApiController(AppDbContext context, IMapper mapper) : ControllerBase
+    {
+        private readonly AppDbContext _context = context;
+        private readonly ResponseDto _response = new ();
+        private readonly IMapper _mapper = mapper;
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Product> productlist = _context.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDto>>(productlist);
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Product product = _context.Products.First(x => x.ProductId == id);
+                _response.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+        [HttpGet]
+        [Route("GetByName{name}")]
+        public ResponseDto GetByCode(string name)
+        {
+            try
+            {
+                Product product = _context.Products.First(x => x.Name.ToLower() == name.ToLower());
+                _response.Result = _mapper.Map<ProductDto>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+        [HttpPost]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Post(ProductDto productDto)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDto);
+                _context.Products.Add(product);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+        [HttpPut]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Put(ProductDto productDto)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDto);
+                _context.Products.Update(product);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+        [HttpDelete]
+        [Route("{id:int}")]
+        [Authorize(Roles = "ADMIN")]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Product product = _context.Products.First(x => x.ProductId == id);
+                _context.Products.Remove(product);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
+            return _response;
+        }
+    }
+}
