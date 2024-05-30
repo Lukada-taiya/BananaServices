f7016196b91cb4c8e77294c37d0d05df0b958fd7
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-30 21:42:14 +0000

    Feat: Created models for shopping carts

---
+++ Banana.Services.ShoppingCartAPI/Models/CartDetails.cs
@@ -0,0 +1,19 @@
+﻿using Banana.Services.ShoppingCartAPI.Models.Dto;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Banana.Services.ShoppingCartAPI.Models
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto ProductDto { get; set; }
+        public int Count { get; set; }
+    }
+}
