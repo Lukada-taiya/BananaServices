9ea4461ec5c2946b07312646db44c07bf60adfaa
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-30 21:43:45 +0000

    Feat: Applied migrations and setup dbcontext configuration

---
+++ Banana.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Banana.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Banana.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; }
+        public DbSet<CartDetails> CartDetails { get; set; }
+        
+    }
+}
---
+++ Banana.Services.ShoppingCartAPI/Migrations/20240530111543_AddCountToCartDetails.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Banana.Services.ShoppingCartAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCountToCartDetails : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Count",
+                table: "CartDetails",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Count",
+                table: "CartDetails");
+        }
+    }
+}
