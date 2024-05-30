2747c3073a693cf59fa84f6ea6e51eb569872033
Lukada-taiya <lukmanadam43@gmail.com>
2024-05-30 21:42:48 +0000

    Feat: Implemented models to be used for the cart system

---
+++ Banana.Services.ShoppingCartAPI/Models/Dto/CartDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Banana.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDto
+    {
+        public CartHeaderDto? CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetailsList { get; set; }
+    }
+}
+ 
