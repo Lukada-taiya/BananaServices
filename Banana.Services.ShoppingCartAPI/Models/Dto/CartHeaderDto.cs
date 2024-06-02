04c157d11031936e181131e677fbdf5dbfdaf091
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-02 12:15:59 +0000

    Enhancement: Modified dto to make couponcode nullable

--- Banana.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
+++ Banana.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -7,7 +7,7 @@ namespace Banana.Services.ShoppingCartAPI.Models.Dto
     { 
         public int CartHeaderId { get; set; }
         public string UserId { get; set; }
-        public string CouponCode { get; set; } 
+        public string? CouponCode { get; set; }
         public double Discount { get; set; } 
         public double CartTotal { get; set; }
     }
