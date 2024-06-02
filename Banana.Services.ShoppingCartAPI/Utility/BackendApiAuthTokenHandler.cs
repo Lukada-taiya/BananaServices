40ec4368e33de678c1e95846775e6e7656a7689f
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-02 12:20:13 +0000

    Feat: Added utitity class to add token to requests sent in services

---
+++ Banana.Services.ShoppingCartAPI/Utility/BackendApiAuthTokenHandler.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Authentication;
+using System.Net.Http.Headers;
+
+namespace Banana.Services.ShoppingCartAPI.Utility
+{
+    public class BackendApiAuthTokenHandler(IHttpContextAccessor contextAccessor) : DelegatingHandler
+    {
+        private readonly IHttpContextAccessor _contextAccessor = contextAccessor;
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _contextAccessor.HttpContext.GetTokenAsync("access_token");
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
