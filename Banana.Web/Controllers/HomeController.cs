e82cb5ebae68b04b6e82f4b08282ce3777da012e
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-02 12:24:25 +0000

    Feat: Added post ProductDetails function to add products to cart

--- Banana.Web/Controllers/HomeController.cs
+++ Banana.Web/Controllers/HomeController.cs
@@ -4,13 +4,15 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Banana.Web.Controllers
 {
-    public class HomeController(ILogger<HomeController> logger, IProductService productService) : Controller
+    public class HomeController(ILogger<HomeController> logger, IProductService productService, ICartService cart) : Controller
     {
         private readonly ILogger<HomeController> _logger = logger;
         private readonly IProductService _productService = productService;
+        private readonly ICartService _cartService = cart;
          
 
         public async Task<IActionResult> Index()
@@ -41,6 +43,38 @@ namespace Banana.Web.Controllers
                 TempData["error"] = response?.Message;
             }
             return View(product);
+        }
+        [Authorize]
+        [HttpPost]
+        [ActionName("ProductDetails")]
+        public async Task<IActionResult> ProductDetails(ProductDto productDto)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub).FirstOrDefault()?.Value;
+            CartHeaderDto cartHeader = new()
+            {
+                UserId = userId
+            };
+            CartDetailsDto cartDetails = new()
+            {
+                Count = productDto.Count,
+                ProductId = productDto.ProductId,
+            };
+            CartDto cart = new()
+            {
+                CartDetailsList = new List<CartDetailsDto> { cartDetails },
+                CartHeader = cartHeader
+            };
+            ResponseDto? response = await _cartService.AddToCart(cart);
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Item added to cart successfully";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(productDto);
         }
 
         public IActionResult Privacy()
