94e8906f106845aa978558baea674b59e6e3845a
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-02 12:25:42 +0000

    Feat: Implemented service to handle all shopping cart related operations

---
+++ Banana.Web/Service/CartService.cs
@@ -0,0 +1,49 @@
+﻿using Banana.Web.Models;
+using Banana.Web.Service.IService;
+using static Banana.Web.Utility.StaticData;
+
+namespace Banana.Web.Service
+{
+    public class CartService(IBaseService baseService) : ICartService
+    {
+        private readonly IBaseService _baseService = baseService;
+        public async Task<ResponseDto> AddToCart(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = cartDto,
+                Url = ShoppingCartApiBase + "/api/cart/CartInsert"
+            });
+        }
+
+        public async Task<ResponseDto> ApplyCoupon(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = cartDto,
+                Url = ShoppingCartApiBase + "/api/cart/ApplyCoupon"
+            });
+        }
+
+        public async Task<ResponseDto> GetCartByUserId(string userId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET, 
+                Url = ShoppingCartApiBase + "/api/cart/GetCart/"+ userId
+            });
+        }
+
+        public async Task<ResponseDto> RemoveFromCart(int cardDetailsId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = cardDetailsId,
+                Url = ShoppingCartApiBase + "/api/cart/CartRemove"
+            });
+        }
+    }
+}
