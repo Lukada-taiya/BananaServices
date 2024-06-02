4ec87b2877f54997d4fa4ec4700e553e1f97ee32
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-02 12:19:11 +0000

    Feat: Implemented Service for Coupons

---
+++ Banana.Services.ShoppingCartAPI/Services/CouponService.cs
@@ -0,0 +1,24 @@
+﻿using Banana.Services.ShoppingCartAPI.Models.Dto;
+using Banana.Services.ShoppingCartAPI.Services.IService;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace Banana.Services.ShoppingCartAPI.Services
+{
+    public class CouponService(IHttpClientFactory httpClientFactory) : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+        public async Task<CouponDto> GetCoupon(string code)
+        {
+            var client = _httpClientFactory.CreateClient("Coupons");
+            var response = await client.GetAsync("/api/coupon/GetByCode/"+code);
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var responseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if (responseDto.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(responseDto.Result));
+            }
+            return new CouponDto();
+        }
+    }
+}
---
+++ Banana.Services.ShoppingCartAPI/Services/IService/ICouponService.cs
@@ -0,0 +1,9 @@
+﻿using Banana.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Banana.Services.ShoppingCartAPI.Services.IService
+{
+    public interface ICouponService
+    {
+        Task<CouponDto> GetCoupon(string code);
+    }
+}
