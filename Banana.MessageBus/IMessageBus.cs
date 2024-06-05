4282bc6c22cf7f5c9da7f2e9071ddb86c92ebc74
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-05 17:55:11 +0000

    Feat: Implemented an azure service bus to queue shopping carts to be emailed

---
+++ Banana.MessageBus/IMessageBus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banana.MessageBus
+{
+    public interface IMessageBus
+    {
+        Task Publish(object  message, string topic_queue_name);
+    }
+}
---
+++ Banana.MessageBus/MessageBus.cs
@@ -0,0 +1,27 @@
+﻿
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace Banana.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        private readonly string connectionString = "--Add Azure Service Bus";
+        public async Task Publish(object message, string topic_queue_name)
+        {
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_name);
+            var serializedmessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage serviceBusMessage = new ServiceBusMessage(Encoding.UTF8.GetBytes(serializedmessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString()
+            };
+
+            await sender.SendMessageAsync(serviceBusMessage);
+            await client.DisposeAsync();
+
+        }
+    }
+}
