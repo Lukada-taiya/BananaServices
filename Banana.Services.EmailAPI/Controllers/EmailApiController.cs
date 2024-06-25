d8da64d95423d77756f99689cf2765e4b89be325
Lukada-taiya <lukmanadam43@gmail.com>
2024-06-25 19:00:06 +0000

    Feat: Added empty api controller

---
+++ Banana.Services.EmailAPI/Controllers/EmailApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Banana.Services.EmailAPI.Controllers
+{
+    public class EmailApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
