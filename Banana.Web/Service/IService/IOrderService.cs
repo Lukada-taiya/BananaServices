72c52a17f423e48c8c883e4d6d3964d078e30ef2
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 18:00:06 +0000

    Feat: Implemented services for OrderApi

---
+++ Banana.Web/Service/IService/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using Banana.Web.Models;
+
+namespace Banana.Web.Service.IService
+{
+    public interface IOrderService
+    { 
+        Task<ResponseDto> CreateOrderAsync(CartDto cartDto); 
+
+    }
+}
