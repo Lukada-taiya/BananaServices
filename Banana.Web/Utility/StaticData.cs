31b49447cf38106aae46882eaac955be05cfdefd
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 18:01:14 +0000

    Feat: Added static values for OrderApi

--- Banana.Web/Utility/StaticData.cs
+++ Banana.Web/Utility/StaticData.cs
@@ -5,6 +5,7 @@
         public static string CouponApiBase { get; set; }
         public static string ProductApiBase { get; set; }
         public static string ShoppingCartApiBase { get; set; }
+        public static string OrderApiBase { get; set; }
         public static string AuthApiBase { get; set; }
         public static string TokenCookie = "JwtToken";
         public static string RoleAdmin = "ADMIN";
