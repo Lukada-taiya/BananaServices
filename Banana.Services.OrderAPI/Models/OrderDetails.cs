532c41c1092050cc439903f3a2cd4dc2378d328a
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 11:16:46 +0000

    Feat: Implemented models for order api

---
+++ Banana.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,21 @@
+﻿using Banana.Services.OrderAPI.Models.Dtos;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Banana.Services.OrderAPI.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public int? OrderDetailsId { get; set; }
+        public int? OrderHeaderId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        public OrderHeader? OrderHeader { get; set; }
+        public int ProductId { get; set; }
+        [NotMapped]
+        public ProductDto? ProductDto { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+    }
+}
