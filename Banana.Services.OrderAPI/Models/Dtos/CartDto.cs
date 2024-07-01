12c2f0e28eae444e28ce14794f3469863c5629c9
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 11:17:20 +0000

    Feat: Added necessary dtos for order api

---
+++ Banana.Services.OrderAPI/Models/Dtos/CartDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Banana.Services.OrderAPI.Models.Dtos
+
+{
+    public class CartDto
+    {
+        public CartHeaderDto? CartHeader { get; set; }
+        public IEnumerable<CartDetailsDto>? CartDetailsList { get; set; }
+    }
+}
+ 
