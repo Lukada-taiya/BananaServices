c6a66924539307d1a22b26949c4dcb5ba5e61e39
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 11:23:24 +0000

    Enhancement: Setup scaffolds and order api pipeline

--- Banana.Services.OrderAPI/Program.cs
+++ Banana.Services.OrderAPI/Program.cs
@@ -1,11 +1,63 @@
+using AutoMapper;
+using Banana.MessageBus;
+using Banana.Services.OrderAPI;
+using Banana.Services.OrderAPI.Data;
+using Banana.Services.OrderAPI.Extensions;
+using Banana.Services.OrderAPI.Services;
+using Banana.Services.OrderAPI.Services.IService;
+using Banana.Services.OrderAPI.Utility;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var mapperConfig = new MapperConfiguration(mc =>
+{
+    mc.AddProfile(new MappingConfig());
+});
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<BackendApiAuthTokenHandler>();
+builder.Services.AddHttpClient("Products", u => u.BaseAddress = new Uri(builder.Configuration["ServiceUrls:Product"])).AddHttpMessageHandler<BackendApiAuthTokenHandler>(); 
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddScoped<IProductService, ProductService>(); 
+builder.Services.AddScoped<IMessageBus, MessageBus>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt => {
+    opt.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Name = "Authorisation",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference =  new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id =  JwtBearerDefaults.AuthenticationScheme
+                }
+            }, Array.Empty<string>()
+        }
+    });
+});
+
+builder.AddAppAuth();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
@@ -17,9 +69,21 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
+ApplyMigration();
 app.Run();
+
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
