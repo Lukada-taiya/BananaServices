9d6a4c3f5a2a77b7362570d83db6be4cde0f72e6
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 11:22:28 +0000

    Feat: Added automapper config

---
+++ Banana.Services.OrderAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Banana.Services.OrderAPI.Models;
+using Banana.Services.OrderAPI.Models.Dtos;
+
+namespace Banana.Services.OrderAPI
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        { 
+            CreateMap<CartHeaderDto,OrderHeaderDto>()
+                .ForMember(dest => dest.OrderTotal, u => u.MapFrom(src => src.CartTotal)).ReverseMap();
+            CreateMap<CartDetailsDto, OrderDetailsDto>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.ProductDto.Name));
+            CreateMap<CartDetailsDto, OrderDetailsDto>()
+                .ForMember(dest => dest.Price, u => u.MapFrom(src => src.ProductDto.Price));
+            CreateMap<OrderDetailsDto, CartDetailsDto>();
+            CreateMap<OrderHeaderDto, OrderHeader>().ReverseMap();
+            CreateMap<OrderDetails, OrderDetailsDto>().ReverseMap();
+        }
+    }
+}
