3a232e51940ae29054dc58c576ebbca603658b4c
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-01 11:21:08 +0000

    Feat: Implemented product services from product api

---
+++ Banana.Services.OrderAPI/Services/ProductService.cs
@@ -0,0 +1,23 @@
+﻿using Banana.Services.OrderAPI.Models.Dtos;
+using Banana.Services.OrderAPI.Services.IService;
+using Newtonsoft.Json;
+
+namespace Banana.Services.OrderAPI.Services
+{
+    public class ProductService(IHttpClientFactory httpClientFactory) : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory = httpClientFactory;
+        public async Task<IEnumerable<ProductDto>> GetAllProducts()
+        {
+            var client = _httpClientFactory.CreateClient("Products");
+            var response = await client.GetAsync("/api/product");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var responseDto = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+            if(responseDto.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<ProductDto>>(Convert.ToString(responseDto.Result));
+            }
+            return new List<ProductDto>();
+        }
+    }
+}
