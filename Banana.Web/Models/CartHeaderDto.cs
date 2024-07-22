63ca2a96d5cef7071a95a052321006b2fc43d634
Lukada-taiya <lukmanadam43@gmail.com>
2024-07-22 23:07:36 +0000

    Enhancement: Added modifications to web dtos

--- Banana.Web/Models/CartHeaderDto.cs
+++ Banana.Web/Models/CartHeaderDto.cs
@@ -7,7 +7,7 @@ namespace Banana.Web.Models
     { 
         public int CartHeaderId { get; set; }
         public string UserId { get; set; }
-        public string CouponCode { get; set; } 
+        public string? CouponCode { get; set; }
         public double Discount { get; set; } 
         public double CartTotal { get; set; }
         public string? Name { get; set; } 
